0c6c09e9710eeacb166e6f0622e0da64597d9c96
Kerecsényi Márk <karatelovag@gmail.com>
2022-09-15 17:47:00 +0200

    Menü kiiratása, menüpont lekérése

--- src/hu/petrik/javabevezeto/Feladat14.java
+++ src/hu/petrik/javabevezeto/Feladat14.java
@@ -1,5 +1,6 @@
 package hu.petrik.javabevezeto;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Feladat14 {
@@ -7,5 +8,17 @@ public class Feladat14 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Kérem adjon meg egy szöveget: ");
         String szoveg = sc.nextLine();
+        String menuPont = "";
+        while(!menuPont.equals("f")) {
+            System.out.println("Kérem adja meg mit szeretne elvégezni a szöveggel:");
+            System.out.println("\ta - Nagy betűssé alakítani");
+            System.out.println("\tb - Kis betűssé alakítani");
+            System.out.println("\tc - Lekérdezni a hosszát");
+            System.out.println("\td - Összehasonlítani egy másik szöveggel");
+            System.out.println("\te - Egy részét kiiratni a szövegnek");
+            System.out.println("\tf - Kilépni");
+            System.out.print("Kérem adja meg az elvégzendő müvelet betűjelét: ");
+            menuPont = sc.nextLine().toLowerCase();
+        }
     }
 }
